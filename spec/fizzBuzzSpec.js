585c2367a2f15bb5faf2dbb2b56dee0e7e847402
Andrew <andy_htun@hotmail.com>
2016-01-04 18:05:19 +0000

    returns FizzBuzz when given divisible 3 & 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -3,4 +3,24 @@ describe("fizzBuzz", function() {
   it("should return Fizz if a number given is divisible by 3 ", function() {
     expect(fizzBuzz(9)).toEqual("Fizz")
   })
+
+  it("should not return Fizz if a number given is not divisible by 3 ", function() {
+    expect(fizzBuzz(10)).not.toEqual("Fizz")
+  })
+
+  it("should return Buzz if a number given is divisible by 5 ", function() {
+    expect(fizzBuzz(10)).toEqual("Buzz")
+  })
+
+  it("should not return Buzz if a number given is not divisible by 5 ", function() {
+    expect(fizzBuzz(11)).not.toEqual("Buzz")
+  })
+
+  it("should return FizzBuzz if a number given is divisible by 3 & 5 ", function() {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz")
+  })
+
+  it("should not return FizzBuzz if a number given is not divisible by 3 & 5 ", function() {
+    expect(fizzBuzz(14)).not.toEqual("FizzBuzz")
+  })
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,9 @@
-function fizzBuzz(number) { if (number % 3 === 0) {
-    return "Fizz";
-}
+function fizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";}
+    else if (number % 5 === 0) {
+      return "Buzz";}
+      else if (number % 3 === 0) {
+        return "Fizz";}
+
 };
